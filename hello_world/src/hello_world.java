57df4878c3c443ca05f3c94b51254f879e2a03ea
Jessica Alabsa <jessica.dougherty@gmail.com>
2019-01-11 20:57:14 -0500

    java projects...

---
+++ hello_world/src/hello_world.java
@@ -0,0 +1,8 @@
+/**
+ * Created by jessicadougherty on 9/28/14.
+ */
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello my beautiful love:*");
+    }
+}
